a00a2a0547edcd03a4b51e88cf3dbef61153f087
unknown <rommel_chua@yahoo.com>
2020-06-26 13:18:06 +1000

    alter Test.java

--- src/main/com/Test.java
+++ src/main/com/Test.java
@@ -1,7 +1,7 @@
 package com;
 public class Test {
 private String prop;
-public String getProp(String t1, String t2, String t3) {
+public String getProp(String t1, String t2, String t3, String t4) {
 	return prop;
 }
 }
